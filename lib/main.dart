4695ec043d5e7ef0486ca0a5e3c81c5c812056f6
faisalrahsu_bankraya <faisal.sulaswanto@bankraya.co.id>
2024-03-17 01:25:12 +0700

    add casters section

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:watchflix/pages/home.dart';
 import 'package:watchflix/pages/login_page.dart';
 import 'package:watchflix/routes/page_routes.dart';
 import 'firebase_options.dart';
